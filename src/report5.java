22f9f261a2643cb48499ebe2716c061eef521a59
e165744 <e165744@ie.u-ryukyu.ac.jp>
2016-12-03 16:52:24 +0900

    step2

--- src/report5.java
+++ src/report5.java
@@ -2,6 +2,15 @@
  * Created by e165744 on 2016/12/03.
  */
 public class report5 {
-    String str = null;
-    System.out.println(str.length());
+    public static void main(String[] args) {
+        String str = null;
+        try{
+            System.out.println(str.length());
+        }
+        catch(NullPointerException e)
+        {
+            System.out.println("strはnullです！");
+            e.printStackTrace();
+        }
+    }
 }
